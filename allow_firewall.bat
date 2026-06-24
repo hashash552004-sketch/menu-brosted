309d3a3f9c68013e52205a0b0e4f021ccba82811
hashash552004-sketch <hashash552004@github.com>
2026-06-24 14:57:10 +0300

    clean: remove db files and bat from tracking

--- allow_firewall.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-netsh advfirewall firewall add rule name="Menu Server 3000" dir=in action=allow protocol=TCP localport=3000
-echo.
-echo ✅ تم فتح المنفذ 3000 في جدار الحماية
-echo الآن جرب تفتح الموقع من هاتفك على http://192.168.1.136:3000
-pause
